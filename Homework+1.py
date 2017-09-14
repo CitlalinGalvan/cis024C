b3cc98811982bad00be63cc75f5c3279e93d5ea4
CitlalinGalvan <31754562+CitlalinGalvan@users.noreply.github.com>
2017-09-13 19:11:38 -0700

    Add files via upload

---
+++ Homework+1.py
@@ -0,0 +1,152 @@
+
+# coding: utf-8
+
+# # excercise 1
+
+# In[1]:
+
+x = 5 
+y = 10
+print x,y
+
+
+# # excercise 2
+
+# In[2]:
+
+x = 10
+y = 20
+z = x + y
+print z
+
+
+# # excersise 3
+
+# 3.1
+
+# In[3]:
+
+a = 5
+b = 6
+print a,b
+
+
+# 3.2
+
+# In[4]:
+
+greeting = "Hello"
+person = "Oscar"
+
+
+# 3.3
+
+# In[5]:
+
+personalGreeting = greeting + person
+print personalGreeting
+
+
+# In[6]:
+
+personalGreeting = greeting + " " + person
+print personalGreeting
+
+
+# 3.4
+
+# In[7]:
+
+int(input("Please enter your Age: "))
+
+
+# 3.5
+
+# In[8]:
+
+age = int(input("Please enter your Age: "))
+
+
+# 3.6
+
+# In[9]:
+
+if age == 35:
+
+
+# 3.7
+
+# In[10]:
+
+if age > 35:
+    print "the age is greater than 35"
+else:
+    print "the age is not greater than 35"
+
+
+# 3.8 Integers
+
+# In[25]:
+
+number = 10
+int(number)
+print number / 4
+
+
+# In[26]:
+
+print number // 4
+
+
+# In[27]:
+
+newNumber = number%4
+print newNumber
+
+
+# 3.9 floats
+
+# In[28]:
+
+float(number)
+number = 101.35
+print number / 4
+
+
+# In[29]:
+
+print number // 4
+
+
+# In[30]:
+
+print number%4
+
+
+# 3.10 Booleans
+
+# In[31]:
+
+myAge = 21
+yourAge = 25
+result = myAge == yourAge
+print result
+
+
+# In[32]:
+
+hisAge = 25
+result = myAge == hisAge
+print result
+
+
+# In[33]:
+
+result = yourAge == hisAge
+print result
+
+
+# In[ ]:
+
+
+
